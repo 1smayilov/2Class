6f42edb3170cf0dc61d4450a6d5d42f1033e788a
Elvin Ismayilov <0utsawg@gmail.com>
2024-03-08 23:29:05 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _2Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Analitycs
+
+            // Assignment
+
+            //  #region Comparision
+            //  bool isGreater = 15 < 20;
+            //  Console.WriteLine(!isGreater);
+            //  #endregion
+
+            // Logical
+
+            // Conditions
+
+
+            Console.WriteLine("Məhsul sayı");
+            int sayi = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Mehsulun alish qiymeti");
+            int alish = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Satilan mehsulun sayı");
+            int satish = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Mehsulun satish qiymeti");
+            int qiymet = int.Parse(Console.ReadLine());
+
+            if ()
+            {
+
+            }
+
+        }
+    }
+}
